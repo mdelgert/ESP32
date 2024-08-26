a97c2ef08c81334e3787ab18e891992b813e84fe
Matthew David Elgert <mdelgert@yahoo.com>
2024-08-26 19:28:38 -0400

    Update serial

--- ESP32-S3-GEEK/README.md
+++ ESP32-S3-GEEK/README.md
@@ -12,9 +12,13 @@ arduino-cli upload --port /dev/ttyACM0 --fqbn esp32:esp32:esp32s3
 #arduino-cli compile -v --fqbn esp32:esp32:esp32s3--output-dir ./build
 #arduino-cli upload --port /dev/ttyACM0 --fqbn esp32:esp32:esp32s3--input-file ./build/Test.ino.bin
 ```
+# Serial
+In Arduino IDE go to tools "USB CDC on Boot Enabled"
+First time may require unplug from USB and plug back in to work
 
 # Links
 https://www.waveshare.com/wiki/ESP32-S3-GEEK#01-WIFI_AP_LCD
 https://files.waveshare.com/wiki/ESP32-S3-GEEK/ESP32-S3-GEEK_Code.zip
 https://mqtt.waveshare.cloud/en/wavesharecloud-wiki.html
 https://waveshare.cloud/
+https://www.reddit.com/r/esp32/comments/114j40i/help_serial_monitor_not_working_with_a_new_esp32/
--- ESP32-S3-GEEK/Tests/Serial/Serial.ino
+++ ESP32-S3-GEEK/Tests/Serial/Serial.ino
@@ -1,18 +1,15 @@
-#include "USB.h"
+//https://www.reddit.com/r/esp32/comments/114j40i/help_serial_monitor_not_working_with_a_new_esp32/
+//In Arduino IDE go to tools "USB CDC on Boot Enabled"
+//First time may require unplug from USB and plug back in to work
 
 void setup() {
-  USB.begin();            // Initialize USB for Serial communication
-  delay(1000);            // Small delay to ensure the serial connection is ready
-
-  Serial.begin(115200);   // Start Serial communication over USB
-  while (!Serial) {       // Wait for Serial to be available
-    delay(10);
-  }
-
-  Serial.println("Hello, ESP32-S3!");
+  Serial.begin(115200);  // Initialize Serial communication at 115200 baud rate
+  delay(1000);           // Small delay to ensure the serial connection is ready
+  //Serial.println("Hello, ESP32-S3!");  // Print a message to the Serial Monitor
 }
 
 void loop() {
   Serial.println("Hello, ESP32-S3!");
-  delay(1000);
+  delay(1000);  // Delay for a second between loop iterations
 }
+
