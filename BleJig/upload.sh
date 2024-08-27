57dc3afdc5010a1322c93b8f2b564ef12e3196ab
Matthew Elgert <mdelgert@yahoo.com>
2024-08-27 00:48:12 -0400

    update bluejig

--- BleJig/upload.sh
+++ BleJig/upload.sh
@@ -1,4 +1,4 @@
 arduino-cli compile --fqbn m5stack:esp32:m5stack_atom
 arduino-cli upload --port /dev/ttyUSB0 --fqbn m5stack:esp32:m5stack_atom
 #arduino-cli compile -v --fqbn m5stack:esp32:m5stack_atom --output-dir ./build
-#arduino-cli upload --port /dev/ttyUSB0 --fqbn m5stack:esp32:m5stack_atom --input-file ./build/BleJiggler.ino.bin
+#arduino-cli upload --port /dev/ttyUSB0 --fqbn m5stack:esp32:m5stack_atom --input-file ./build/BleJig.ino.bin
