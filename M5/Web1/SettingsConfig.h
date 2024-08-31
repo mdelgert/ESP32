238847c955bce818ad4afc220243bda32851e8b8
Matthew Elgert <mdelgert@yahoo.com>
2024-08-30 23:10:05 -0400

    Add web1.ino

---
+++ M5/Web1/SettingsConfig.h
@@ -0,0 +1,21 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Default configuration values for setup_mode = true
+#define SETUP_MODE true
+#define DEFAULT_SSID "ESP32WAP"
+#define DEFAULT_PASSWORD "12345678"
+#define DEFAULT_DEVICE_NAME "atom1"
+#define DEFAULT_ADMIN "admin"
+#define DEFAULT_ADMIN_PASSWORD "password"
+
+// Default mqtt configuration
+#define MQTT_BROKER "example.emqx.com"
+#define MQTT_PORT 1883
+#define MQTT_TOPIC "test/topic/1"
+#define MQTT_USER "user"
+#define MQTT_PASSWORD "password"
+#define MQTT_CERTIFICATE_ENABLE false
+#define MQTT_CERTIFICATE ""
+
+#endif // CONFIG_H
