a003ed3b7c48943c68a8320a1115008b850a3284
Matthew Elgert <mdelgert@yahoo.com>
2024-09-04 12:32:05 -0400

    Add first tft example

--- T-Dongle-S3/Tests/TftImage1/TftImage1.ino
+++ T-Dongle-S3/Tests/TftImage1/TftImage1.ino
@@ -1,6 +1,21 @@
+#include "Arduino.h"
+#include "logo.h"
+#include "TFT_eSPI.h" // https://github.com/Bodmer/TFT_eSPI
 #include "Config.h"
 
+TFT_eSPI tft = TFT_eSPI();
+
 void setup() {
+  pinMode(TFT_LEDA_PIN, OUTPUT);
+  // Initialise TFT
+  tft.init();
+  //tft.setRotation(1);
+  tft.setRotation(3); // Set rotation to flip the screen
+  tft.fillScreen(TFT_DARKGREY);
+  digitalWrite(TFT_LEDA_PIN, 0);
+  tft.setTextFont(1);
+  tft.setTextColor(TFT_GREEN, TFT_BLACK);
+  tft.pushImage(0, 0, 160, 80, (uint16_t *)gImage_logo);
 }
 
 void loop() {
